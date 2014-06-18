a2f65f51b97f67cd37bbe2166d7158f17219712e
Lia Kitamura <kit4mura@gmail.com>
2014-06-18 14:13:42 -0600

    made post model

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Post.create(title: 'My first title', author: 'Lia Kitamura', description: 'Kenny G is crazy')
+Post.create(title: 'My second title', author: 'Lia Kitamura', description: 'Kenny G is crazy')
+Post.create(title: 'My third title', author: 'Lia Kitamura', description: 'Kenny G is crazy')
+Post.create(title: 'My fourth title', author: 'Lia Kitamura', description: 'Kenny G is crazy')
+Post.create(title: 'My fifth title', author: 'Lia Kitamura', description: 'Kenny G is crazy')
